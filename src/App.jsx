c0fdad1f9697d5bba8c8e4e56cfb01770fdb9470
Matias Salazar <matias39974593@gmail.com>
2024-08-22 13:29:31 -0300

    agregamos un color de fondo en el body

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
   return (
     <>
       <main className='container my-5'>
-    <h1 className='text-center'>
+    <h1 className='text-center text-light'>
       Bienvenido
     </h1>
-    <h2 className='text-center'>
+    <h2 className='text-center text-light'>
       Ingresa tus tareas
     </h2>
     <FormularioTarea></FormularioTarea>
