2f2c111c23deec4f6a3ea0c2a77926a7e543d2e2
Matias Salazar <matias39974593@gmail.com>
2024-09-10 15:52:04 -0300

    agregamos una funcion para borrar tareas con el nombre de la tarea

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,9 +1,9 @@
 import { ListGroup, Button } from "react-bootstrap";
 
-const ItemTarea = ({ tarea }) => {
+const ItemTarea = ({ tarea, borrarTarea }) => {
   return (
     <ListGroup.Item className="d-flex justify-content-between">
-      {tarea} <Button variant="warning">Borrar</Button>
+      {tarea} <Button variant="warning" onClick={()=> borrarTarea(tarea)}>Borrar</Button>
     </ListGroup.Item>
   );
 };
