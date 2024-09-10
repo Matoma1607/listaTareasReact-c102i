d3c98ec818fb03a81b85956011ba576689b31b8e
Matias Salazar <matias39974593@gmail.com>
2024-09-10 12:17:01 -0300

    solo se dio formato al documento en ItemTarea

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,9 +1,11 @@
 import { ListGroup, Button } from "react-bootstrap";
 
-const ItemTarea = ({tarea}) => {
-    return (
-        <ListGroup.Item className="d-flex justify-content-between">{tarea} <Button variant="warning">Borrar</Button></ListGroup.Item>
-    );
+const ItemTarea = ({ tarea }) => {
+  return (
+    <ListGroup.Item className="d-flex justify-content-between">
+      {tarea} <Button variant="warning">Borrar</Button>
+    </ListGroup.Item>
+  );
 };
 
 export default ItemTarea;
